97e8b46de4482d46f0568851583215a96b95d279
Trevor Jones <tjones009@yahoo.com>
2020-02-27 15:47:17 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,7 +14,11 @@ def monopoly_with_second_tier
   # complexity.
   
   monopoly = {
-    railroads: {}
+    railroads: {
+      pieces: 4,
+      names:{},
+      rent_in_dollars: {}
+    }
   }
   
 end
