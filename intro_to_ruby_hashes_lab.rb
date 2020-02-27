2c911b855d5cd8da21d057fb4f6ba41da426292f
Trevor Jones <tjones009@yahoo.com>
2020-02-27 16:16:51 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,9 +28,9 @@ def monopoly_with_third_tier
       },
       rent_in_dollars: {
         :one_piece_owned => 25,
-        :two_piece_owned => 50,
-        :three_piece_owned => 100,
-        :four_piece_owned => 200
+        :two_pieces_owned => 50,
+        :three_pieces_owned => 100,
+        :four_pieces_owned => 200
       }
     }
   }
@@ -62,8 +62,8 @@ def monopoly_with_fourth_tier
       },
       rent_in_dollars: {
         :one_piece_owned => 25,
-        :two_piece_owned => 50,
-        :three_piece_owned => 100,
+        :two_pieces_owned => 50,
+        :three_pieces_owned => 100,
         :four_piece_owned => 200
       }
     }
