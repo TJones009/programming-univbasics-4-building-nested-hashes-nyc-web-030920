bae46e792f04e9016a8f3e260822bb3d64c1e83f
Trevor Jones <tjones009@yahoo.com>
2020-02-27 16:12:44 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,17 +1,10 @@
 def monoopoly
-  # Remember implicit return! We're returning this new Hash _without_ the
-  # keyword "return." Nice and neat.
-  
   monopoly = {
     railroads: {}
   }
 end
 
 def monopoly_with_second_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
   
   monopoly = {
     railroads: {
@@ -20,14 +13,9 @@ def monopoly_with_second_tier
       rent_in_dollars: {}
     }
   }
-  
 end
 
 def monopoly_with_third_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
   
    monopoly = {
     railroads: {
@@ -38,6 +26,7 @@ def monopoly_with_third_tier
         :b_and_o_railroad => {},
         :shortline => {}
       },
+      rent_in_dollars: {
         :one_peice_owned => 25,
         :two_peice_owned => 50,
         :three_peice_owned => 100,
